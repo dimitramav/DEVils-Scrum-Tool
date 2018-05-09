c5d8ea8fa5997d20886cfa63d1cfd1d7cde216a7
Orestis Garmpis <37930690+Pantokratoras7@users.noreply.github.com>
2018-05-09 05:50:38 +0300

    Update Project and User classes
    Minor changes in User class

--- ScrumTool/src/main/java/ys09/model/User.java
+++ ScrumTool/src/main/java/ys09/model/User.java
@@ -45,119 +45,94 @@ public class User {
       this.password = password;
     }
 
+    // Getters
     public int getId() {
       return idUser;
     }
-
     public String getUsername() {
       return username;
     }
-
     public String getEmail() {
       return mail;
     }
-
     public String getFirstName() {
       return firstname;
     }
-
     public String getLastName() {
       return lastname;
     }
-
     public String getPassword() {
       return password;
     }
-
-    public void setPassword(String password) {
-      this.password = password;
-    }
-
     public String getCountry() {
       return country;
     }
-
     public String getJob() {
       return job;
     }
-
     public String getCompany() {
       return company;
     }
-
     public String getDescription() {
       return description;
     }
-
     public String getBio() {
       return bio;
     }
-
     public String getPhoto() {
       return photo;
     }
-
     public int getIsAdmin() {
       return isAdmin;
     }
-
-    public void setIsAdmin(int isAdmin) {
-      this.isAdmin = isAdmin;
-    }
-
     public int getNumOfProjects() {
       return numProjects;
     }
 
-    public void setNumOfProjects(int numProjects) {
-      this.numProjects = numProjects;
-    }
-
+    // Setters
     public void setIdUser(int idUser) {
         this.idUser = idUser;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public void setMail(String mail) {
         this.mail = mail;
     }
-
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
-
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-
+    public void setPassword(String password) {
+      this.password = password;
+    }
     public void setCountry(String country) {
         this.country = country;
     }
-
     public void setJob(String job) {
         this.job = job;
     }
-
     public void setCompany(String company) {
         this.company = company;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public void setBio(String bio) {
         this.bio = bio;
     }
-
     public void setPhoto(String photo) {
         this.photo = photo;
     }
-
     public void setNumProjects(int numProjects) {
+        this.numProjects = numProjects;
+    }
+    public void setIsAdmin(int isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+    public void setNumOfProjects(int numProjects) {
         this.numProjects = numProjects;
     }
 }
