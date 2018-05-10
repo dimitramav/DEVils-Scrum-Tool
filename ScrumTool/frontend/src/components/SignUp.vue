5679323e97d66d233c10622f13f0fa3491ee9d39
YolandaKok <yolanda.kokkinou@gmail.com>
2018-05-11 01:47:09 +0300

    Webapp web.xml SOS

--- ScrumTool/frontend/src/components/SignUp.vue
+++ ScrumTool/frontend/src/components/SignUp.vue
@@ -17,7 +17,7 @@ export default {
   methods: {
     // Fetches posts when the component is created.
     callRestService () {
-      axios.get(`http://localhost:8765/app/api/projects`, { headers: { auth: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsYWxhQGdtYWlsLmNvbSJ9.xoWiGcX5ekJsXF6gkFze16JmLODQW0oCGHgQ462lr_OE2Dt_yKmacI_3iQ4NVKAabDunmst066c2xtFI25uhnw' } })
+      axios.get(`http://localhost:8765/app/api/projects`, { headers: { auth: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsYWxhQGdtYWlsLmNvbSJ9.s-cqurwwavJEr8KE5vinX6TroN-1GXwWvI0YZTRtCRk4FT6fB3uSiZ08nwZEY3bKBFbC4eWhupzTUkxjfLNBYA' } })
         .then(response => {
           // JSON responses are automatically parsed.
           this.response = response.data
