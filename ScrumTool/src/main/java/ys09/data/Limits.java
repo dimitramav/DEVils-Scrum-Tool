249fad79dc84ab84e43460cb55319e088540ab7e
Orestis Garmpis <37930690+Pantokratoras7@users.noreply.github.com>
2018-05-15 05:33:23 +0300

    Add pagination
    Use of limited projects per request and depending on deadline date (sent to front end sorted)

--- ScrumTool/src/main/java/ys09/data/Limits.java
+++ ScrumTool/src/main/java/ys09/data/Limits.java
@@ -1,24 +1,27 @@
 package ys09.data;
 
+import java.util.Date;
+
+
 public class Limits {
 
     public static final int DEFAULT_COUNT = 10;
     public static final int TOTAL_NOT_FETCHED = -1;
 
-    private final long start;
+    private final Date start;
     private final int  count;
     private long total = TOTAL_NOT_FETCHED;
 
-    public Limits(long start) {
-        this(start, DEFAULT_COUNT);
-    }
+    //public Limits(String start) {
+    //    this(start, DEFAULT_COUNT);
+    //}
 
-    public Limits(long start, int count) {
+    public Limits(int count, Date start) {
         this.start = start;
         this.count = count;
     }
 
-    public long getStart() {
+    public Date getStart() {
         return start;
     }
 
