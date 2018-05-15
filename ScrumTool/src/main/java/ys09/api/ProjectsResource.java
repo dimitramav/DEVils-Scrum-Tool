d3b0af9796c5452433fe9c46d7b37b44920105e7
Orestis Garmpis <37930690+Pantokratoras7@users.noreply.github.com>
2018-05-15 05:30:04 +0300

    Add pagination
    Use of limited projects per request and depending on deadline date (sent to front end sorted)

--- ScrumTool/src/main/java/ys09/api/ProjectsResource.java
+++ ScrumTool/src/main/java/ys09/api/ProjectsResource.java
@@ -1,28 +1,37 @@
 package ys09.api;
 
-import org.restlet.data.Status;
-import org.restlet.representation.Representation;
-import org.restlet.resource.ResourceException;
-import org.restlet.resource.ServerResource;
 import ys09.auth.CustomAuth;
 import ys09.conf.Configuration;
 import ys09.data.DataAccess;
 import ys09.data.Limits;
 import ys09.model.Project;
-import java.io.Reader;
+
+import org.restlet.data.Status;
+import org.restlet.representation.Representation;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.ServerResource;
 import org.restlet.resource.Post;
 import org.restlet.ext.json.JsonRepresentation;
-import org.json.JSONObject;
-import com.google.gson.Gson;
 import org.restlet.representation.BufferingRepresentation;
 import org.restlet.data.ClientInfo;
 import org.restlet.engine.header.Header;
 import org.restlet.util.Series;
+import org.json.JSONObject;
+
+import java.io.Reader;
 import java.io.IOException;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import com.google.gson.Gson;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Date;
+import java.util.Locale;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.ConcurrentMap;
 import org.restlet.Message;
 import org.restlet.Context;
@@ -45,9 +54,18 @@ public class ProjectsResource extends ServerResource {
 
         // Get UserId
         String userId = getRequestAttributes().get("userId").toString();
-        String limit = getRequestAttributes().get("limit").toString();
-        String offset = getRequestAttributes().get("offset").toString();
         int user = Integer.parseInt(userId);
+
+        // Read the values of url (limit and nearestDeadline) for pagination
+        String strcount = getQuery().getValues("limit");
+        String strstart = getQuery().getValues("nearestDeadline");
+        DateFormat format = new SimpleDateFormat("yyyy-M-d");
+        Date expDate = new Date(Long.MAX_VALUE);
+        try { expDate = format.parse(strstart); }
+        catch (Exception e) { System.out.println("No date"); }
+        System.out.println(expDate);
+        System.out.println(strcount);
+        Limits limit = new Limits(Integer.parseInt(strcount), expDate);
 
         // Access the headers of the request !
         /*Series requestHeaders = (Series)getRequest().getAttributes().get("org.restlet.http.headers");
@@ -80,7 +98,7 @@ public class ProjectsResource extends ServerResource {
 
         // Set the response headers
         //return new JsonMapRepresentation(map);*/
-        List<Project> projects = dataAccess.getUserProjects(user);
+        List<Project> projects = dataAccess.getUserProjects(user, limit);
         map.put("results", projects);
         return new JsonMapRepresentation(map);
     }
