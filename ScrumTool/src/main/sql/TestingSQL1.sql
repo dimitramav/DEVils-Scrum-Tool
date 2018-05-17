47e6efc038b57c3b6a767650eeb10c0c485a836b
Pantokratoras7 <orestispgarmpis@yahoo.com>
2018-05-17 15:53:36 +0300

    Simple commit

--- ScrumTool/src/main/sql/TestingSQL1.sql
+++ ScrumTool/src/main/sql/TestingSQL1.sql
@@ -1,12 +1,12 @@
 use ScrumTool_db;
 select * from Project;
-insert into Project (title, isDone, deadlineDate) values ('Scrum', FALSE, '2017-07-15');
+insert into Project (title, isDone, deadlineDate) values ('Scrum', FALSE, '2018-07-15');
 insert into Project (title, isDone, deadlineDate) values ('Facebook', FALSE, '2018-08-16');
 insert into Project (title, isDone, deadlineDate) values ('Twitter', FALSE, '2019-09-17');
-insert into User (username, password, mail, firstname, lastname, isAdmin, numProjects)
-	values ('orestis', 'garmpis', 'sdi1400025@di.uoa.gr', 'Orestis', 'Garmpis', True, 2);
-insert into User (username, password, mail, firstname, lastname, isAdmin, numProjects)
-	values ('kostas', 'kotronis', 'sdi1400074@di.uoa.gr', 'Kostas', 'Kotronis', False, 1);
+insert into User (password, mail, firstname, lastname, isAdmin, numProjects)
+	values ('garmpis', 'sdi1400025@di.uoa.gr', 'Orestis', 'Garmpis', True, 2);
+insert into User (password, mail, firstname, lastname, isAdmin, numProjects)
+	values ('kotronis', 'sdi1400074@di.uoa.gr', 'Kostas', 'Kotronis', False, 1);
 select * from User;
 insert into Project_has_User (Project_id, User_id, role) values (2, 1, 'Product Owner');
 insert into Project_has_User (Project_id, User_id, role) values (3, 2, 'Developer');
