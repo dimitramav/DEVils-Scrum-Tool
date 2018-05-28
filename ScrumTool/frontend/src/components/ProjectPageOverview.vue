911a03e378bbae69bf089be097c93308bac329fc
KostasKotronis <kostaskotronis1996@gmail.com>
2018-05-29 01:04:46 +0300

    project page overview vol2

--- ScrumTool/frontend/src/components/ProjectPageOverview.vue
+++ ScrumTool/frontend/src/components/ProjectPageOverview.vue
@@ -3,9 +3,12 @@
     <b-container class="Navigation" fluid>
 
       <navbar></navbar>
-      <sidebar></sidebar>
+      <!--<sidebar></sidebar>-->
 
       <br>
+      <b-row>
+        <b-breadcrumb :items="items" style="position: relative;left: 41px;"/>
+      </b-row>
       <b-row style="padding-top:10px;">
         <b-col class="text-left">
           <h2>Insurance App</h2>
@@ -18,42 +21,104 @@
         <b-container>
           <b-row>
             <b-col>
-              <b-jumbotron>
+              <br>
                 <b-row>
                   <h2>Current sprint</h2>
                 </b-row>
+                <br>
                 <b-row>
                   <b-col>
                     <b-card title="TODO">
-
+                      <p class="card-text">1024</p>
                     </b-card>
                   </b-col>
                   <b-col>
                     <b-card title="DOING">
-
+                      <p class="card-text">5</p>
                     </b-card>
                   </b-col>
                   <b-col>
                     <b-card title="DONE">
-
+                      <p class="card-text">15</p>
                     </b-card>
                   </b-col>
                 </b-row>
-                <br>
-                <b-card>
-                  <p style="text-align: left">Days Remaining:</p>
+                <br><br><br>
+                <b-row>
+                <b-col>
+                <b-card no-body class="text-left">
+                  <p class="card-text">Days Remaining:  15</p>
                 </b-card>
+                </b-col>
+                  <b-col></b-col>
+                </b-row>
+                <br><br><br>
+                <b-row style="padding-top:10px;">
+                  <b-col class="text-left">
+                    <h5>Progress</h5>
+                  </b-col>
+                </b-row>
+                <b-progress :value="value" show-progress class="mb-3"></b-progress>
                 <br>
                 <b-button variant="primary">Go to Sprint Page</b-button>
-                <br>
-              </b-jumbotron>
+              <br><br>
             </b-col>
             <b-col>
-              <b-jumbotron>
+              <br>
                 <b-row>
                   <h2>Team</h2>
                 </b-row>
-              </b-jumbotron>
+                <br>
+                <b-list-group>
+                  <b-list-group-item class="flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">Product Owner</h5>
+                    </div>
+                    <p align="left">
+                     Name: Steve Jobs
+                      <br>
+                      Email: unavailable
+                    </p>
+                  </b-list-group-item>
+                  <b-list-group-item class="flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">Scrum Master</h5>
+                    </div>
+                    <p align="left">
+                      Name: Bill Gates
+                      <br>
+                      Email: unavailable
+                    </p>
+                  </b-list-group-item>
+                  <b-list-group-item class="flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">Developer</h5>
+                    </div>
+                    <p align="left">
+                      Name: Evaggelos Raptis
+                      <br>
+                      Email: unavailable
+                    </p>
+                  </b-list-group-item>
+                </b-list-group>
+              <br>
+              <template>
+                <div>
+                  <b-form inline>
+                   Add User &nbsp;
+                    <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" placeholder="email" />
+                    as &nbsp;
+                    <b-form-select class="mb-2 mr-sm-2 mb-sm-0"
+                                   :value="null"
+                                   :options="{ '1': 'Scrum Master', '2': 'Developer'}"
+                                   id="inlineFormCustomSelectPref">
+                      <option slot="first" :value="null">Choose role</option>
+                    </b-form-select>
+                    <b-button variant="primary">Invite</b-button>
+                  </b-form>
+                </div>
+              </template>
+              <br><br>
             </b-col>
           </b-row>
         </b-container>
@@ -65,12 +130,39 @@
 <script>
 import Sidebar from "./Sidebar.vue"
 import Navbar from "./Navbar.vue"
-  export default {
-    components: {
-      navbar: Navbar,
-      sidebar: Sidebar,
+
+export default {
+  components: {
+    navbar: Navbar,
+    sidebar: Sidebar,
+  },
+  data() {
+    return {
+      value: 75,
+      selected: [], // Must be an array reference!
+      options: [{
+        text: 'User Story 1',
+        value: 'orange'
+      }, {
+        text: 'User Story 2',
+        value: 'apple'
+      }, {
+        text: 'User Story 3',
+        value: 'pineapple'
+      }],
+      items: [{
+        text: 'Home',
+        href: '#'
+      }, {
+        text: 'Insurance App',
+        href: '#'
+      }, {
+        text: 'Overview',
+        active: true
+      }]
     }
   }
+}
 
 </script>
 
