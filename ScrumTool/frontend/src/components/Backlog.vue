043c9418a2894bb4cdaeabec243f5a87965ba9d2
Mark Varvagiannis <markosvarv@gmail.com>
2018-05-31 00:25:19 +0300

    Backlog.vue -open one collapse

--- ScrumTool/frontend/src/components/Backlog.vue
+++ ScrumTool/frontend/src/components/Backlog.vue
@@ -6,8 +6,12 @@
           Deadline: {{cur_project.deadlineDate}}
         </p>
         <div>
-          <b-btn v-b-toggle.collapse1 variant="primary">Toggle Collapse</b-btn>
-          <b-collapse id="collapse1" class="mt-2">
+          <b-btn  @click="showCollapse=cur_project.title"
+                  class="collapsed"
+                 aria-controls="collapse">
+            Toggle Collapse
+          </b-btn>
+          <b-collapse class="mt-2" v-model="showCollapse===cur_project.title" id="collapse">
             <b-card>
               <p class="card-text">Collapse contents Here</p>
             </b-card>
@@ -37,6 +41,7 @@ export default {
       currentProjects: [],
       doneProjects: [],
       teamData: json.team,
+      showCollapse: null,
     }
   },
   methods: {
