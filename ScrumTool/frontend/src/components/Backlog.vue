40094d7e8eb4d1e810253f88e9bf472180959072
dimitramav <dmavrof@gmail.com>
2018-06-04 13:04:59 +0300

    update epic modal

--- ScrumTool/frontend/src/components/Backlog.vue
+++ ScrumTool/frontend/src/components/Backlog.vue
@@ -10,23 +10,23 @@
             <b-form inline style="margin: 10px;" @submit="newEpic">
               <h4>Title:</h4>
               <label class="sr-only" for="newEpicName"></label>
-              <b-form-input id="newEpicName" placeholder="New Epic's Name" v-model="form.newTitle" required/>
+              <b-form-input id="newEpicName" placeholder="New Epic's Name" v-model="form.newEpicTitle" required/>
               <h5>Description:</h5>
               <label class="sr-only" for="newEpicDescription"></label>
-              <b-form-input id="newEpicDescription" placeholder="New Epic's Description" v-model="form.newDescription" required/>
+              <b-form-input id="newEpicDescription" placeholder="New Epic's Description" v-model="form.newEpicDesc" required/>
               <h5>Priority:</h5>
               <b-row class="text-center">
                 <b-col>
                   <label for="high">High</label>
-                  <input type="radio" name="myChoice" id="high" value="High" v-model="priority_picked" required>
+                  <input type="radio" name="myChoice" id="high" value="High" v-model="pick_priority" required>
                 </b-col>
                 <b-col>
                   <label for="medium">Medium</label>
-                  <input type="radio" name="myChoice" id="medium" value="Medium" v-model="priority_picked" required>
+                  <input type="radio" name="myChoice" id="medium" value="Medium" v-model="pick_priority" required>
                 </b-col>
                 <b-col>
                   <label for="low">Low</label>
-                  <input type="radio" name="myChoice" id="low" value="Low" v-model="priority_picked" required>
+                  <input type="radio" name="myChoice" id="low" value="Low" v-model="pick_priority" required>
                 </b-col>
               </b-row>
               <b-button variant="success" type="submit" style="margin-top: 10px; width: 100%;">Add to Epics</b-button>
@@ -44,35 +44,82 @@
               <h5 >Epic</h5>
             </b-col>
             <b-col class="text-right">
-              <a  href="https://www.w3schools.com/tags/tag_a.asp"><u>Edit</u></a>
+              <!-- EDIT EPIC -->
+              <b-btn v-b-modal="'modal'+cur_pbi.idPBI">Edit</b-btn>
+              <b-modal :id="'modal'+cur_pbi.idPBI" title="Update Epic">
+                <div class="text-left">
+                  <b-form @submit="onSubmit" @reset="onReset">
+                    <b-form-group id="updateEpic1"
+                                  label="Title:"
+                                  label-for="updateEpicTitle">
+                      <b-form-input id="updateEpicTitle"
+                                    type="text"
+                                    v-model="form.updateEpicTitle"
+                                    required
+                                    :placeholder="cur_pbi.title">
+                      </b-form-input>
+                    </b-form-group>
+                    <b-form-group id="updateEpic2"
+                                  label="Description:"
+                                  label-for="updateEpicDesc">
+                      <b-form-input id="updateEpicDesc"
+                                    type="text"
+                                    v-model="form.updateEpicDesc"
+                                    required
+                                    :placeholder="cur_pbi.description">
+                      </b-form-input>
+                    </b-form-group>
+                    <h7>Priority:</h7>
+                    <b-row class="text-center">
+                      <b-col>
+                        <label for="high">High</label>
+                        <input type="radio" name="myChoice" id="high_up" value="High" v-model="update_priority" required>
+                      </b-col>
+                      <b-col>
+                        <label for="medium">Medium</label>
+                        <input type="radio" name="myChoice" id="medium_up" value="Medium" :v-model="update_priority" required>
+                      </b-col>
+                      <b-col>
+                        <label for="low">Low</label>
+                        <input type="radio" name="current_priority" id="low_up" value="Low" :v-model="update_priority" required>
+                      </b-col>
+                    </b-row>
+                  </b-form>
+                </div>
+              </b-modal>
             </b-col>
           </b-row>
         </div>
-        <p class="card-text">
-          {{cur_pbi.description}}
-        </p>
-        <b-btn v-b-toggle="'icollapse'+cur_pbi.idPBI" v-on:click="getEpicUserStories(cur_pbi.idPBI)" variant="primary">User Stories</b-btn>
-        <b-collapse :id="'icollapse'+cur_pbi.idPBI" class="mt-2">
-        <div>
-          <b-card-group v-for="cur_us in currentUserStories[cur_pbi.idPBI]" :key="cur_us.idPBI" deck style="margin: 0 auto;float: none;margin-bottom: 10px;">
-            <b-card class="mb-1">
-            <b-card-header header-tag="header" class="p-1" role="tab">
-              <b-btn block href="#"  v-b-toggle="'collapse'+cur_us.idPBI" variant="info">{{cur_us.title}}</b-btn>
-            </b-card-header>
-            <b-collapse :id="'collapse'+cur_us.idPBI" visible accordion="my-accordion" role="tabpanel">
-              <b-card-body>
-                <p class="card-text">
-                   {{cur_us.description}}
-                </p>
-              </b-card-body>
-            </b-collapse>
-          </b-card>
-          </b-card-group>
-        </div>
-        </b-collapse>
-        <div slot="footer">
-          <h6 class="text-muted">{{cur_pbi.priority}}</h6>
-        </div>
+        <p class=" card-text
+              ">
+              {{cur_pbi.description}}
+              </p>
+              <b-btn v-b-toggle="'collapse'+cur_pbi.idPBI" v-on:click="getEpicUserStories(cur_pbi.idPBI)"
+                     variant="primary">User Stories
+              </b-btn>
+              <b-collapse :id="'collapse'+cur_pbi.idPBI" class="mt-2">
+                <div>
+                  <b-card-group v-for="cur_us in currentUserStories[cur_pbi.idPBI]" :key="cur_us.idPBI" deck
+                                style="margin: 0 auto;float: none;margin-bottom: 10px;">
+                    <b-card class="mb-1">
+                      <b-card-header header-tag="header" class="p-1" role="tab">
+                        <b-btn block href="#" v-b-toggle="'collapse'+cur_us.idPBI" variant="info">{{cur_us.title}}
+                        </b-btn>
+                      </b-card-header>
+                      <b-collapse :id="'collapse'+cur_us.idPBI" visible accordion="my-accordion" role="tabpanel">
+                        <b-card-body>
+                          <p class="card-text">
+                            {{cur_us.description}}
+                          </p>
+                        </b-card-body>
+                      </b-collapse>
+                    </b-card>
+                  </b-card-group>
+                </div>
+              </b-collapse>
+              <div slot="footer">
+                <h6 class="text-muted">{{cur_pbi.priority}}</h6>
+              </div>
       </b-card>
     </b-card-group>
   </b-row>
@@ -91,10 +138,11 @@ export default {
   data() {
     return {
       form: {
-        newTitle: '',
-        newDescription: '',
+        newEpicTitle: '',
+        newEpicDesc: '',
       },
-      priority_picked: '',
+      pick_priority: '',
+      update_priority: '',
       logOut: null,
       currentPbis: [],
       currentUserStories: [[],[]],
@@ -138,12 +186,12 @@ export default {
     newEpic (evt) {
       evt.preventDefault();
       const self = this;
-      this.priority_picked=self.priorityToNumber(this.priority_picked);
+      this.pick_priority=self.priorityToNumber(this.pick_priority);
       let config = {
         headers: {"auth": localStorage.getItem('auth_token'), "Content-Type": 'application/json'}
       }
       let data = {
-        title: this.form.newTitle, description: this.form.newDescription, priority:this.priority_picked, Project_id: this.currentProject_id,
+        title: this.form.newEpicTitle, description: this.form.newEpicDesc, priority:num_priority, Project_id: this.currentProject_id,
       }
       axios.post('http://localhost:8765/app/api/users/' + localStorage.getItem('userId') + '/projects/' + this.currentProject_id + '/pbis?isEpic=true', data, config)
         .then(function (response) {
