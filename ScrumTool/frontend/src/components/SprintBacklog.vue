b380afb6a016aef14463c4a12ac6c92cc6f3daca
Mark Varvagiannis <markosvarv@gmail.com>
2018-07-05 16:16:29 +0300

    fix drag n drop - no db

--- ScrumTool/frontend/src/components/SprintBacklog.vue
+++ ScrumTool/frontend/src/components/SprintBacklog.vue
@@ -3,135 +3,141 @@
 
     <navbar :dashboard="true"></navbar>
     <br>
-      <!--Sprint Infos-->
+    <!--Sprint Infos-->
+    <b-row>
+      <b-col class="center">
+        <h1>Sprint #{{currentSprint.numSprint}}</h1>
+      </b-col>
+    </b-row>
+    <hr>
+    <br>
+    <b-row>
+      <b-col>
+        <h4><i class="text-muted">Sprint Goal: </i>{{currentSprint.goal}}</h4>
+      </b-col>
+      <b-col>
+        <h4><i class="text-muted">Sprint Plan: </i>{{currentSprint.plan}}</h4>
+      </b-col>
+      <b-col>
+        <h4><i class="text-muted">Deadline Date: </i>{{currentSprint.deadlineDate}} </h4>
+      </b-col>
+    </b-row>
+    <hr style="margin-bottom:0px">
+    <br>
+    <!--board-->
+    <!--<b-container class="bv-example-row bv-example-row-flex-cols">-->
+    <b-row>
+      <b-col>
+        <h2>User Stories</h2>
+      </b-col>
+      <b-col>
+        <h2>TO DO</h2>
+      </b-col>
+      <b-col>
+        <h2>DOING</h2>
+      </b-col>
+      <b-col>
+        <h2>DONE</h2>
+      </b-col>
+    </b-row>
+    <div v-for="cur_story in currentStories" :key="cur_story.idPBI" deck style="margin-bottom: 10px; padding-left: 10px;" deck class="mb-2">
       <b-row>
-        <b-col class="center">
-          <h1>Sprint #{{currentSprint.numSprint}}</h1>
+        <b-col>
+          <b-card :title="cur_story.title" img-top tag="article" class="mb-2" >
+
+            <div slot="header"  >
+              <b-row>
+
+                <!--New Task-->
+                <div>
+                  <b-btn  v-b-modal="'new_task'+cur_story.idPBI">Add Task</b-btn>
+                  <b-modal @ok="addTask(cur_story.idPBI)" class="text-left" :id="'new_task'+cur_story.idPBI" title="Add new task">
+                    <b-form>
+                      <b-form-group label="Description:" :label-for="'addTask'+cur_story.idPBI">
+                        <b-form-input :id="'addTask'+cur_story.idPBI"
+                                      type="text"
+                                      v-model="newTask_form.desc"
+                                      required>
+                        </b-form-input>
+                      </b-form-group>
+                    </b-form>
+                  </b-modal>
+                </div>
+              </b-row>
+            </div>
+          </b-card>
+        </b-col>
+
+
+        <b-col class="w-25 bg-light">
+          <draggable v-model="todoTasks[cur_story.idPBI]" @change="addToTodo" :options="{group:'UserStories'}">
+            <transition-group name="markos1" class="list-group">
+              <b-card-group v-for="cur_task in todoTasks[cur_story.idPBI]"  :key="cur_task.idTask" deck style="margin-bottom: 10px; padding-left: 10px;" deck class="mb-2">
+                <div v-show="cur_task.state===1">
+                  <b-card img-top tag="article" class="mb-2">
+                    <div slot="header"  >
+                      <b-row>
+                        <div>
+                          <b-btn  v-b-modal="'edit_task'+cur_task.idTask">Edit Task</b-btn>
+                        </div>
+                      </b-row>
+                    </div>
+                    <p class="card-text">{{cur_task.description}}</p>
+                  </b-card>
+                </div>
+              </b-card-group>
+            </transition-group>
+          </draggable>
+        </b-col>
+        <b-col>
+
+          <draggable v-model="doingTasks[cur_story.idPBI]" @change="addToDoing" :options="{group:'UserStories'}">
+            <transition-group name="markos" class="list-group">
+              <b-card-group v-for="cur_task in doingTasks[cur_story.idPBI]"  :key="cur_task.idTask" deck style="margin-bottom: 10px; padding-left: 10px;" deck class="mb-2">
+
+                <div v-show="cur_task.state===2">
+                  <b-card img-top tag="article" class="mb-2">
+                    <div slot="header"  >
+                      <b-row>
+                        <div>
+                          <b-btn  v-b-modal="'edit_task'+cur_task.idTask">Edit Task</b-btn>
+                        </div>
+                      </b-row>
+                    </div>
+                    <p class="card-text">{{cur_task.description}}</p>
+                  </b-card>
+                </div>
+              </b-card-group>
+            </transition-group>
+          </draggable>
+        </b-col>
+        <b-col class="w-25 bg-light">
+
+
+          <draggable v-model="doneTasks[cur_story.idPBI]" @change="addToDone" :options="{group:'UserStories'}">
+            <transition-group name="yolanda" class="list-group">
+
+              <b-card-group v-for="cur_task in doneTasks[cur_story.idPBI]"  :key="cur_task.idTask" deck style="margin-bottom: 10px; padding-left: 10px;" deck class="mb-2">
+                <div v-show="cur_task.state===3">
+                  <b-card img-top tag="article" class="mb-2">
+                    <div slot="header"  >
+                      <b-row>
+                        <div>
+                          <b-btn  v-b-modal="'edit_task'+cur_task.idTask">Edit Task</b-btn>
+                        </div>
+                      </b-row>
+                    </div>
+                    <p class="card-text">{{cur_task.description}}</p>
+                  </b-card>
+                </div>
+              </b-card-group>
+            </transition-group>
+          </draggable>
         </b-col>
       </b-row>
       <hr>
-      <br>
-      <b-row>
-        <b-col>
-          <h4><i class="text-muted">Sprint Goal: </i>{{currentSprint.goal}}</h4>
-        </b-col>
-        <b-col>
-          <h4><i class="text-muted">Sprint Plan: </i>{{currentSprint.plan}}</h4>
-        </b-col>
-        <b-col>
-          <h4><i class="text-muted">Deadline Date: </i>{{currentSprint.deadlineDate}} </h4>
-        </b-col>
-      </b-row>
-      <hr style="margin-bottom:0px">
-      <br>
-      <!--board-->
-      <!--<b-container class="bv-example-row bv-example-row-flex-cols">-->
-        <b-row>
-          <b-col>
-            <h2>User Stories</h2>
-          </b-col>
-          <b-col>
-            <h2>TO DO</h2>
-          </b-col>
-          <b-col>
-            <h2>DOING</h2>
-          </b-col>
-          <b-col>
-            <h2>DONE</h2>
-          </b-col>
-        </b-row>
-        <div v-for="cur_story in currentStories" :key="cur_story.idPBI" deck style="margin-bottom: 10px; padding-left: 10px;" deck class="mb-2">
-          <b-row>
-            <b-col>
-              <b-card :title="cur_story.title" img-top tag="article" class="mb-2" >
-
-                <div slot="header"  >
-                  <b-row>
-
-                    <!--New Task-->
-                    <div>
-                      <b-btn  v-b-modal="'new_task'+cur_story.idPBI">Add Task</b-btn>
-                      <b-modal @ok="addTask(cur_story.idPBI)" class="text-left" :id="'new_task'+cur_story.idPBI" title="Add new task">
-                        <b-form>
-                          <b-form-group label="Description:" :label-for="'addTask'+cur_story.idPBI">
-                            <b-form-input :id="'addTask'+cur_story.idPBI"
-                                          type="text"
-                                          v-model="newTask_form.desc"
-                                          required>
-                            </b-form-input>
-                          </b-form-group>
-                        </b-form>
-                      </b-modal>
-                    </div>
-                  </b-row>
-                </div>
-              </b-card>
-            </b-col>
-
-
-            <b-col class="w-25 bg-light">
-              <draggable v-model="todoTasks" @change="addToTodo" :options="{group:'UserStories'}">
-                <p v-if="todoTasks.length===0">Drag here</p>
-                <b-card-group v-for="cur_task in todoTasks"  :key="cur_task.idTask" deck style="margin-bottom: 10px; padding-left: 10px;" deck class="mb-2">
-                  <div v-show="cur_task.state===1 && cur_task.PBI_id===cur_story.idPBI">
-                    <b-card img-top tag="article" class="mb-2">
-                      <div slot="header"  >
-                        <b-row>
-                          <div>
-                            <b-btn  v-b-modal="'edit_task'+cur_task.idTask">Edit Task</b-btn>
-                          </div>
-                        </b-row>
-                      </div>
-                      <p class="card-text">{{cur_task.description}}</p>
-                    </b-card>
-                  </div>
-                </b-card-group>
-              </draggable>
-            </b-col>
-            <b-col>
-              <draggable v-model="doingTasks" @change="addToDoing" :options="{group:'UserStories'}">
-                <p v-if="doingTasks.length===0">Drag here</p>
-
-                <b-card-group v-for="cur_task in doingTasks"  :key="cur_task.idTask" deck style="margin-bottom: 10px; padding-left: 10px;" deck class="mb-2">
-                  <div v-show="cur_task.state===2 && cur_task.PBI_id===cur_story.idPBI">
-                    <b-card img-top tag="article" class="mb-2">
-                      <div slot="header"  >
-                        <b-row>
-                          <div>
-                            <b-btn  v-b-modal="'edit_task'+cur_task.idTask">Edit Task</b-btn>
-                          </div>
-                        </b-row>
-                      </div>
-                      <p class="card-text">{{cur_task.description}}</p>
-                    </b-card>
-                  </div>
-                </b-card-group>
-              </draggable>
-            </b-col>
-            <b-col class="w-25 bg-light">
-              <draggable v-model="doneTasks" @change="addToDone" :options="{group:'UserStories'}">
-                <p v-if="doneTasks.length===0">Drag here</p>
-
-                <b-card-group v-for="cur_task in doneTasks"  :key="cur_task.idTask" deck style="margin-bottom: 10px; padding-left: 10px;" deck class="mb-2">
-                  <div v-show="cur_task.state===3 && cur_task.PBI_id===cur_story.idPBI">
-                    <b-card img-top tag="article" class="mb-2">
-                      <div slot="header"  >
-                        <b-row>
-                          <div>
-                            <b-btn  v-b-modal="'edit_task'+cur_task.idTask">Edit Task</b-btn>
-                          </div>
-                        </b-row>
-                      </div>
-                      <p class="card-text">{{cur_task.description}}</p>
-                    </b-card>
-                  </div>
-                </b-card-group>
-              </draggable>
-            </b-col>
-          </b-row>
-          <hr>
-        </div>
-      <!--</b-container>-->
+    </div>
+    <!--</b-container>-->
   </b-container>
 </template>
 
@@ -156,10 +162,10 @@
         currentProject_id:'',
         today:'',
         //currentTasks:[[3],[]],
-        todoTasks:[],
+        todoTasks: [],
         doingTasks:[],
         doneTasks: [],
-        draggedTask: null,
+        lastItemNull: null,
       }
     },
 
@@ -215,6 +221,7 @@
             console.log(error);
           })
       },
+
       getSprintStories() {
         const self = this;
         axios.get(this.$url + 'users/' + localStorage.getItem('userId') + '/projects/' + this.currentProject_id + '/stories?isCurrent=true', {
@@ -248,32 +255,35 @@
               }
             }
             else if (response.data.tasks) {
-              //console.log(response.data.tasks);
-              response.data.tasks.forEach(function(element) {
+              console.log(response.data.tasks);
 
+              response.data.tasks.forEach(function(element) {
+                self.todoTasks[element.PBI_id]=[];
+                self.doingTasks[element.PBI_id]=[];
+                self.doneTasks[element.PBI_id]=[];
+              });
+
+              response.data.tasks.forEach(function(element) {
                 switch(element.state) {
                   case 1:
-                    self.todoTasks.push(element);
+                    self.todoTasks[element.PBI_id].push(element);
                     break;
                   case 2:
-                    self.doingTasks.push(element);
+                    // if (typeof(self.doingTasks[element.PBI_id])=== "undefined") self.doingTasks[element.PBI_id] = [element];
+                    self.doingTasks[element.PBI_id].push(element);
                     break;
                   case 3:
-                    self.doneTasks.push(element);
+                    //if (typeof(self.doneTasks[element.PBI_id])=== "undefined") self.doneTasks[element.PBI_id] = [element];
+                    self.doneTasks[element.PBI_id].push(element);
                     break;
                   default:
                     console.log ("Unknown value " + element.state);
                 }
-
-
-                //console.log(element);
-                // if (typeof(self.currentTasks[element.state-1])=== "undefined") self.currentTasks[element.state-1] = [element];
-                // else self.currentTasks[element.state-1].push(element);
-
               });
               console.log(self.todoTasks);
               console.log(self.doingTasks);
               console.log(self.doneTasks);
+
               //console.log(response.data.tasks);
               /* let result = response.data.tasks.reduce(function (r, a) {
                  r[a.PBI_id] = r[a.PBI_id] || [];
@@ -316,7 +326,10 @@
               else console.log(error);
             }
             if (response.data.task) {
-              self.currentTasks.push(response.data.task);
+              console.log(self.todoTasks);
+              console.log(storyId);
+              console.log(response.data.task);
+              self.todoTasks[storyId].push(response.data.task);
             }
           })
           .catch(function (error) {
@@ -346,6 +359,7 @@
     },
 
     mounted(){
+      console.log("mphkaaaaa");
       this.currentProject_id=parseInt(this.$route.params.id);
       this.getSprintInfos();
       this.getSprintStories();
@@ -374,5 +388,30 @@
   }
   .modal-backdrop.in {
     opacity: 0.9;
+  }
+
+  .flip-list-move {
+    transition: transform 0.5s;
+  }
+
+  .no-move {
+    transition: transform 0s;
+  }
+
+  .ghost {
+    opacity: .5;
+    background: #C8EBFB;
+  }
+
+  .list-group {
+    min-height: 100px;
+  }
+
+  .list-group-item {
+    cursor: move;
+  }
+
+  .list-group-item i{
+    cursor: pointer;
   }
 </style>
