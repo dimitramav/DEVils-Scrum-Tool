4eb1a043fe5f9bce9d272a1b27728cb3648d8a48
ogarmpis <orestispgarmpis@yahoo.com>
2020-04-27 16:57:28 +0300

    Create a new empty client with babel, eslint, testing

---
+++ client/src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
+
+Vue.prototype.$url = "http://localhost:8765/api/";
+
+new Vue({
+    router,
+    render: (h) => h(App),
+}).$mount("#app");
---
+++ client/vue.config.js
@@ -0,0 +1,17 @@
+// vue configuration file
+module.exports = {
+    // Proxy does not work for axios (bug)
+    devServer: {
+        /*proxy: {
+            '^/api/': {
+                target: 'http://localhost:8765',
+                pathRewrite: { '^/api/': '/api/' },
+                ws: true,
+                changeOrigin: true,
+                logLevel: 'debug',
+            }
+        },*/
+        //https: true,
+        port: 9000,
+    },
+};
